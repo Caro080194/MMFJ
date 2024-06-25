83ff9fb6e221fc2d0205b3f142e5585bc639ca0e
Caroline Le Ny <caroline.lenygoyat@gmail.com>
2024-06-25 16:56:47 -0400

    cors update

--- server/index.js
+++ server/index.js
@@ -2,14 +2,23 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
+
+// CORS options to allow requests from frontend domain
+const corsOptions = {
+  origin: 'https://motion-mind-fitness-journey-mvmaf95ur-caroline-le-nys-projects.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
 //require the handlers
 const { signUp, login, postPRs, youtube, trackLastPR } = require('./handlers');
 
 express()
+  .use(cors(corsOptions)) // Add CORS middleware with the options
   .use(function (req, res, next) {
     res.header(
       'Access-Control-Allow-Methods',
@@ -27,19 +36,18 @@ express()
   .use(express.urlencoded({ extended: false }))
   .use('/', express.static(__dirname + '/'))
 
-  //USERS ENDPOINTS_______________________________________________________________
+  // USERS ENDPOINTS_______________________________________________________________
 
-  //signUp -> post a new user to the users collection in mongo db
+  // signUp -> post a new user to the users collection in mongo db
   .post('/signUp', signUp)
-  //login -> retrieve info the user from db and login the user 
+  // login -> retrieve info the user from db and login the user 
   .post('/login', login)
-  //postPRs -> allowed the user to post his new PRs in the PRs collection to be able to track his achievment
+  // postPRs -> allowed the user to post his new PRs in the PRs collection to be able to track his achievment
   .post('/postPRs', postPRs)
-  //trackNewPR -> Get the last PR the user posted to update in real time his PR
+  // trackNewPR -> Get the last PR the user posted to update in real time his PR
   .get('/trackLastPR', trackLastPR)
 
-
-  //access to the youtube key for the frontend
+  // access to the youtube key for the frontend
   .get('/pre-workout/movement', youtube)
   .get('/pre-workout/bodyArea', youtube)
   .get('/post-workout/movement', youtube)
